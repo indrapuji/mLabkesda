95199828ce1999fc77f98ba01c599463e36cfeac
Indra Puji Novirwan <indrapuji@gmail.com>
2022-07-22 14:12:31 +0700

    tulis pesan sudah

--- src/screens/WriteScreen.js
+++ src/screens/WriteScreen.js
@@ -7,6 +7,7 @@ import {
   Image,
   TextInput,
   Dimensions,
+  ScrollView,
 } from 'react-native';
 import React, {useState} from 'react';
 import {leftArrow, camera} from '../assets';
@@ -21,89 +22,119 @@ const WriteScreen = ({navigation}) => {
     image: '',
   });
   return (
-    <SafeAreaView>
-      <View style={{position: 'relative'}}>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flexDirection: 'column', flex: 1}}>
         <View>
-          <View
-            style={{
-              position: 'relative',
-              justifyContent: 'center',
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginTop: 16,
-            }}
-          >
-            <TouchableOpacity
+          <View>
+            <View
               style={{
+                position: 'relative',
+                justifyContent: 'center',
                 flexDirection: 'row',
                 alignItems: 'center',
-                position: 'absolute',
-                left: 16,
+                marginTop: 16,
               }}
-              onPress={() => navigation.goBack()}
             >
-              <Image source={leftArrow} />
-            </TouchableOpacity>
-            <View>
-              <Text style={{fontWeight: '700', fontSize: 20, color: '#3AB4F2'}}>
-                Tulis Pesan
-              </Text>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  position: 'absolute',
+                  left: 16,
+                }}
+                onPress={() => navigation.goBack()}
+              >
+                <Image source={leftArrow} />
+              </TouchableOpacity>
+              <View>
+                <Text
+                  style={{fontWeight: '700', fontSize: 20, color: '#3AB4F2'}}
+                >
+                  Tulis Pesan
+                </Text>
+              </View>
             </View>
           </View>
+          <ScrollView>
+            <View style={{marginHorizontal: 16, marginTop: 20}}>
+              <Text>Kepada</Text>
+              <TextInput
+                placeholder="Kepada"
+                autoCapitalize="none"
+                style={styles.inputSize}
+                onChangeText={text => setValue({...value, kepada: text})}
+                value={value.kepada}
+              />
+            </View>
+            <View style={{marginHorizontal: 16, marginTop: 10}}>
+              <Text>Subject</Text>
+              <TextInput
+                placeholder="Subject"
+                autoCapitalize="none"
+                style={styles.inputSize}
+                onChangeText={text => setValue({...value, subject: text})}
+                value={value.subject}
+              />
+            </View>
+            <View style={{marginHorizontal: 16, marginTop: 10}}>
+              <Text>Pesan</Text>
+              <TextInput
+                multiline
+                placeholder="Pesan"
+                autoCapitalize="none"
+                style={styles.inputSizeText}
+                onChangeText={text => setValue({...value, isiPesan: text})}
+                value={value.isiPesan}
+              />
+            </View>
+            {value.image ? (
+              <View
+                style={{
+                  width: width - 32,
+                  backgroundColor: 'grey',
+                  height: (width - 32) / 1.5,
+                  marginLeft: 16,
+                  marginTop: 16,
+                }}
+              />
+            ) : (
+              <TouchableOpacity
+                style={{
+                  marginTop: 10,
+                  width: width - 32,
+                  height: 50,
+                  backgroundColor: 'yellow',
+                  marginHorizontal: 16,
+                  borderRadius: 10,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Image source={camera} style={{width: 20, height: 20}} />
+                <Text style={{fontWeight: '700', color: '#3AB4F2'}}>
+                  Attach Image
+                </Text>
+              </TouchableOpacity>
+            )}
+          </ScrollView>
         </View>
-        <View style={{marginHorizontal: 16, marginTop: 20}}>
-          <Text>Kepada</Text>
-          <TextInput
-            placeholder="Kepada"
-            autoCapitalize="none"
-            style={styles.inputSize}
-            onChangeText={text => setValue({...value, kepada: text})}
-            value={value.kepada}
-          />
-        </View>
-        <View style={{marginHorizontal: 16, marginTop: 10}}>
-          <Text>Subject</Text>
-          <TextInput
-            placeholder="Subject"
-            autoCapitalize="none"
-            style={styles.inputSize}
-            onChangeText={text => setValue({...value, subject: text})}
-            value={value.subject}
-          />
-        </View>
-        <View style={{marginHorizontal: 16, marginTop: 10}}>
-          <Text>Pesan</Text>
-          <TextInput
-            multiline
-            placeholder="Pesan"
-            autoCapitalize="none"
-            style={styles.inputSizeText}
-            onChangeText={text => setValue({...value, isiPesan: text})}
-            value={value.isiPesan}
-          />
-        </View>
-        <TouchableOpacity
-          style={{
-            marginTop: 10,
-            width: width - 32,
-            height: 50,
-            backgroundColor: 'yellow',
-            marginHorizontal: 16,
-            borderRadius: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Image source={camera} style={{width: 20, height: 20}} />
-          <Text style={{fontWeight: '700', color: '#3AB4F2'}}>
-            Attach Image
-          </Text>
-        </TouchableOpacity>
-        {/* <View style={{position: 'absolute', bottom: 10}}>
-          <TouchableOpacity>
-            <Text>Kirim</Text>
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          <TouchableOpacity
+            style={{
+              width: width - 32,
+              marginLeft: 16,
+              height: 50,
+              borderRadius: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'green',
+            }}
+          >
+            <Text style={{fontSize: 20, fontWeight: '700', color: 'white'}}>
+              Kirim
+            </Text>
           </TouchableOpacity>
-        </View> */}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -130,6 +161,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingLeft: 20,
     paddingRight: 20,
+    paddingTop: 20,
     color: 'blue',
   },
 });
